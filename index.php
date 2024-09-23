b82ead338dd52f43ed600afe7cbc5133d5cbacd3
Krystian Jagiełło <113018723+jagiellokrystian@users.noreply.github.com>
2024-09-23 16:35:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Skrypt PHP</title>
+</head>
+<body>
+    <p>
+        <?php
+        $liczbaCalkowita = 52;
+        $liczbaRzeczywista = 10.50;
+        $ciagZnakow = "liczba";
+
+        echo "wartosc liczby calkowitej: $liczbaCalkowita <br />";
+        echo "wartosc liczby rzeczywistej: $liczbaRzeczywista <br />";
+        echo "wartosc ciagu znaku: $ciagZnakow <br />";
+        ?>
+    </p>
+    
+</body>
+</html>
